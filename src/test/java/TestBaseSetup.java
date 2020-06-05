6b554aa235d3e33d0f4b6f7209e9186efdc75fab
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2020-06-05 21:16:07 +0300

    lesson5

--- src/test/java/TestBaseSetup.java
+++ src/test/java/TestBaseSetup.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -25,16 +26,16 @@ public class TestBaseSetup {
         optionsCh.addArguments("--disable-notifications");
         optionsCh.addArguments("--window-size=1300,1080");
         FirefoxOptions optionFF = new FirefoxOptions();
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
         context.setAttribute("driver", driver);
-       /* try {
+        try {
             //driver = new RemoteWebDriver(new URL("http://ec2-3-133-138-12.us-east-2.compute.amazonaws.com:4444/wd/hub"), optionsCh);
             //driver = new RemoteWebDriver(new URL("127.0.0.1:4444/wd/hub"), options);
             //driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), options);
-            //driver = new RemoteWebDriver(new URL("localhost:4444/wd/hub"), options);
+            driver = new RemoteWebDriver(new URL("localhost:4444/wd/hub"), optionsCh);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 
     @AfterMethod
